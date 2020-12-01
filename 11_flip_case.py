022f390dc3b00b579fda18619b6cb3958c7f7a6f
Alan Tseng <atseng202@gmail.com>
2020-11-30 17:28:58 -0800

    Finish 11-15 problems

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -1,18 +1,21 @@
 def flip_case(phrase, to_swap):
     """Flip [to_swap] case each time it appears in phrase.
 
-        >>> flip_case('Aaaahhh', 'a')
-        'aAAAhhh'
+    >>> flip_case('Aaaahhh', 'a')
+    'aAAAhhh'
 
-        >>> flip_case('Aaaahhh', 'A')
-        'aAAAhhh'
+    >>> flip_case('Aaaahhh', 'A')
+    'aAAAhhh'
 
-        >>> flip_case('Aaaahhh', 'h')
-        'AaaaHHH'
+    >>> flip_case('Aaaahhh', 'h')
+    'AaaaHHH'
 
     """
     letters = list(phrase)
     for idx in range(0, len(letters)):
-        letter = letter[idx]
+        letter = letters[idx]
+        # check if letter needs to be swapped
         if letter.lower() == to_swap.lower():
+            letters[idx] = letters[idx].swapcase()
 
+    return "".join(letters)
--- 13_capitalize.py
+++ 13_capitalize.py
@@ -1,10 +1,12 @@
 def capitalize(phrase):
     """Capitalize first letter of first word of phrase.
 
-        >>> capitalize('python')
-        'Python'
+    >>> capitalize('python')
+    'Python'
 
-        >>> capitalize('only first word')
-        'Only first word'
+    >>> capitalize('only first word')
+    'Only first word'
     """
-
+    phrase_list = list(phrase)
+    phrase_list[0] = phrase_list[0].capitalize()
+    return "".join(phrase_list)
